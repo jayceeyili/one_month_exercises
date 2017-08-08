6773819cec0378441dc5ca7e81c82480ccc5a810
Li Yi <jayceeyili@gmail.com>
2017-08-07 21:27:20 -0700

    initial solution for fibs

--- coding_exercises/fibs.js
+++ coding_exercises/fibs.js
@@ -3,28 +3,28 @@
   Solve it both iteratively and recursively.
 *****************************************************************************************************/
 // iterative solution
-// const fibs = n => {
-//   let result = [1, 1];
-//   if (n < 3) return result;
-//
-//   let i = 1;
-//   while (result.length < n) {
-//     let sum = result[i - 1] + result[i];
-//     i++;
-//     result.push(sum);
-//   }
-//
-//   return result;
-// };
+const fibs = n => {
+  let result = [1, 1];
+  if (n < 3) return result;
+
+  let i = 1;
+  while (result.length < n) {
+    let sum = result[i - 1] + result[i];
+    i++;
+    result.push(sum);
+  }
+
+  return result;
+};
 
 // recursive solution
-const fibs = (n, arr = [1, 1]) => {
-  if (arr.length >= n) return arr;
-  let length = arr.length - 1;
-
-  let sum = arr[length] + arr[length - 1];
-  arr.push(sum);
-  return fibs(n, arr);
-}
+// const fibs = (n, arr = [1, 1]) => {
+//   if (arr.length >= n) return arr;
+//   let length = arr.length - 1;
+//
+//   let sum = arr[length] + arr[length - 1];
+//   arr.push(sum);
+//   return fibs(n, arr);
+// }
 
 console.log(fibs(3));
