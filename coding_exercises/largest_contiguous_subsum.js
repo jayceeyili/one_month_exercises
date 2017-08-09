3c76c293fa5f7649d1df7a03e22cb98d8eead858
Li Yi <jayceeyili@gmail.com>
2017-08-09 12:51:18 -0700

    complete largest_contiguous_subsum

--- coding_exercises/largest_contiguous_subsum.js
+++ coding_exercises/largest_contiguous_subsum.js
@@ -3,3 +3,21 @@
   You can solve this trivially in O(n**2) time by considering all subarrays.
   Try to solve it in O(n) time with O(1) memory.
 ***************************************************************************************************/
+
+const largest_contiguous_subsum = arr => {
+  let largestSum = null;
+  let currSum = 0;
+
+  arr.forEach(n => {
+    currSum += n;
+    if (currSum < 0) {
+      currSum = 0;
+    } else if (currSum > largestSum || !largestSum) {
+      largestSum = currSum;
+    }
+  })
+
+  return largestSum;
+};
+
+console.log(largest_contiguous_subsum([-3, 0, 2 , -1, 3]));
