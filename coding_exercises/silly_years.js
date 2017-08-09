235a97ab2bf46e7be12ab187883b12ece028b29b
Li Yi <jayceeyili@gmail.com>
2017-08-09 13:07:26 -0700

    complete silly_years

--- coding_exercises/silly_years.js
+++ coding_exercises/silly_years.js
@@ -5,3 +5,25 @@
   1978 => 19 + 78 = 97
   2307 => 23 + 07 = 30
 ***************************************************************************************************/
+
+const silly_years = year => {
+  let result = [];
+
+
+  while (result.length < 10) {
+    if (isSilly(year)) result.push(year);
+    year++;
+  }
+
+  return result;
+};
+
+const isSilly = year => {
+  let yearStr = year.toString();
+  let firstTwo = yearStr.slice(0,2);
+  let middleTwo = yearStr.slice(1, 3);
+  let lastTwo = yearStr.slice(2);
+  return parseInt(firstTwo) + parseInt(lastTwo) === parseInt(middleTwo);
+};
+
+console.log(silly_years(1900));
