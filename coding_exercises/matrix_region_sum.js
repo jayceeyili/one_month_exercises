92b0c6ce16a6432f6b0070bf748369fdccb45054
Li Yi <jayceeyili@gmail.com>
2017-08-09 13:30:05 -0700

    add requirments for day four exercises

---
+++ coding_exercises/matrix_region_sum.js
@@ -0,0 +1,4 @@
+/***************************************************************************************************
+  Given a matrix of integers and coordinates of a rectangular region within the matrix,
+  find the sum of numbers falling inside the rectangle.
+***************************************************************************************************/
---
+++ coding_exercises/productify.js
@@ -0,0 +1,4 @@
+/***************************************************************************************************
+  Given a list of numbers in an array, replace all the numbers with the product of all other numbers.
+  Do this in O(n) time without using division.
+***************************************************************************************************/
