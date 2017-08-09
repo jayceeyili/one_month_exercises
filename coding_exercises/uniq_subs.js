10fb23c890ca4c82f06d3dbc136ab6970107163b
Li Yi <jayceeyili@gmail.com>
2017-08-09 12:25:40 -0700

    complete uniq_subs

--- coding_exercises/uniq_subs.js
+++ coding_exercises/uniq_subs.js
@@ -1,3 +1,22 @@
 /***************************************************************************************************
   Write a method that finds all the unique substrings for a word.
 ***************************************************************************************************/
+
+const uniq_subs = str => {
+  let subsMap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    for (let k = i; k < str.length; k++) {
+      let currSub = str.slice(i, k + 1);
+      if (!subsMap[currSub]) {
+        subsMap[currSub] = true;
+      } else {
+        subsMap[currSub] = !subsMap[currSub];
+      }
+    }
+  }
+
+  return Object.keys(subsMap);
+};
+
+console.log(uniq_subs('abcd'));
