763d8fe23552acdf9103814eacbadd516d062735
Li Yi <jayceeyili@gmail.com>
2017-08-09 14:16:06 -0700

    complete merge_sort

--- coding_exercises/merge_sort.js
+++ coding_exercises/merge_sort.js
@@ -1,3 +1,33 @@
 /***************************************************************************************************
   Implement merge sort.
 ***************************************************************************************************/
+
+const merge_sort = arr => {
+  if (arr.length < 2) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(merge_sort(left), merge_sort(right));
+};
+
+const merge = (left, right) => {
+  let indexLeft = 0;
+  let indexRight = 0;
+  let merged = [];
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      merged.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      merged.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+
+  return [...merged, ...left.slice(indexLeft), ...right.slice(indexRight)];
+};
+
+console.log(merge_sort([2, 41, 11, 454, 1]));
