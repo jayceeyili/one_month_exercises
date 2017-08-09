2d0238defac5e3b42624b2fdcf67bedadce4858d
Li Yi <jayceeyili@gmail.com>
2017-08-09 13:49:44 -0700

    complete matrix_region_sum

--- coding_exercises/matrix_region_sum.js
+++ coding_exercises/matrix_region_sum.js
@@ -2,3 +2,15 @@
   Given a matrix of integers and coordinates of a rectangular region within the matrix,
   find the sum of numbers falling inside the rectangle.
 ***************************************************************************************************/
+
+const matrix_region_sum = (matrix, topLeftIndex, bottomRightIndex) => {
+  let sum = 0;
+
+  for (let i = topLeftIndex[0]; i <= bottomRightIndex[0]; i++) {
+    for (let k = topLeftIndex[1]; k <= bottomRightIndex[1]; k++) {
+      sum += matrix[i][k];
+    }
+  }
+
+  return sum;
+};
