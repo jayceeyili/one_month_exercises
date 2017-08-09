17251e35cf6b55cb372d66b463b8bc41c7139cc9
Li Yi <jayceeyili@gmail.com>
2017-08-09 13:23:23 -0700

    complete pair_sum

--- coding_exercises/pair_sum.js
+++ coding_exercises/pair_sum.js
@@ -2,3 +2,25 @@
   Given an array of integers, return all pairs that sum up to a specified value k.
   List the pairs in [min, max] order.
 ***************************************************************************************************/
+
+const pair_sum = (arr, sum) => {
+  let result = [];
+  let sumMap = {};
+
+  arr.forEach(num => {
+    let target = sum - num;
+    if (sumMap[target]) {
+      if (num < target) {
+        result.push([num, target]);
+      } else {
+        result.push([target, num]);
+      }
+    }
+
+    sumMap[num] = num;
+  })
+
+  return result;
+};
+
+console.log(pair_sum([3, 5, 2, -4, 8, 11], 7));
