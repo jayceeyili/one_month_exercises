aa118a54e1e5fad05a0eb7c10f805ab80c30c310
Li Yi <jayceeyili@gmail.com>
2017-08-15 11:52:31 -0700

    description for look_amd_say

--- coding_exercises/look_and_say.js
+++ coding_exercises/look_and_say.js
@@ -1,3 +1,17 @@
 /***************************************************************************************************
+  Implement the 'look and say' function. 'Look and say' takes an input array and outputs
+  an array that describes the count of the elements in the input array as they appear in order.
+  Example:
+  # there is one '1' in the input array
+  look_and_say([1]) == [[1, 1]]
 
+  # there are two '1's in the input array
+  look_and_say([1, 1]) == [[2, 1]]
+
+  # there is one '2', followed by one '1' in the input array
+  look_and_say([2, 1]) == [[1, 2], [1, 1]]
+
+  # is one '1', followed by one '2', followed by 2 '1's in the input
+  # array
+  look_and_say([1, 2, 1, 1]) == [[1, 1], [1, 2], [2, 1]]
 ***************************************************************************************************/
