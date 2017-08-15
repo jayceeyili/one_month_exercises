3e3898461799e111451742d57be86872c80486b2
Li Yi <jayceeyili@gmail.com>
2017-08-15 12:10:49 -0700

    complete part1 of non_comparison_sorts

--- coding_exercises/non_comparison_sorts.js
+++ coding_exercises/non_comparison_sorts.js
@@ -5,6 +5,14 @@
   Sort this array in O(n + N) time. You may use O(N) memory.
   Part 3: Say I give you an array of n strings, each of length k. I claim that, using merge sort,
   you can sort this in O(knlog(n)), since comparing a pair of strings takes O(k) time.
-  I want you to beat that. Sort the strings in O(kn). Hint: do not compare any two strings. 
+  I want you to beat that. Sort the strings in O(kn). Hint: do not compare any two strings.
   You may assume all strings contain only lowercase letters a..z without whitespace or punctuation.
 ***************************************************************************************************/
+
+const non_comparison_sort1 = (arr, n) => {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+  return result;
+};
