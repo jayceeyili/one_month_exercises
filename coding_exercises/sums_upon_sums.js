85b92a067bae09ff3e0cac080af82e6d00652824
Li Yi <jayceeyili@gmail.com>
2017-08-16 13:19:12 -0700

    complete sums_upon_sums

--- coding_exercises/sums_upon_sums.js
+++ coding_exercises/sums_upon_sums.js
@@ -5,6 +5,31 @@
   Finally, how could you solve the problem in O(n), and also O(1) space?
 ***************************************************************************************************/
 
-const sums_upon_sums = arr => {
-  
+// O(nlog(n))
+const sums_upon_sums1 = (arr, n) => {
+  arr = arr.sort();
+  for (let i = 0; i <= n; i++) {
+    if (i !== arr[i]) return i;
+  }
+
+  return null;
 };
+
+// O(n);
+const sums_upon_sums2 = (arr, n) => {
+  let helperArr = new Array(n).fill(false);
+  for (let i = 0; i < arr.length; i++) {
+    helperArr[arr[i]] = !helperArr[arr[i]];
+  }
+
+  for (let k = 0; k <= n; k++) {
+    if (!helperArr[k]) {
+      return k;
+    }
+  }
+
+  return null;
+};
+
+// O(n) in time, O(1) in space; Gauss Sum!
+const sums_upon_sums3 = (arr, n) => (1 + n) * n / 2 - arr.reduce((a, b) => a + b);
