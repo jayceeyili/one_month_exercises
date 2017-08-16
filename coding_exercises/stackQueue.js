7fdd93ce89cc58d8c5ee97a8b26532b0b04ba688
Li Yi <jayceeyili@gmail.com>
2017-08-16 14:36:23 -0700

    complete stackQueue

--- coding_exercises/stackQueue.js
+++ coding_exercises/stackQueue.js
@@ -3,3 +3,32 @@
   In terms of performance, enqueue should be O(1), but dequeue may be worst-case O(n).
   In terms of ammortized time, dequeue should be O(1). Prove that your solution accomplishes this.
 ***************************************************************************************************/
+
+class StackQueue {
+  constructor() {
+    this.leftStack = [];
+    this.rightStack = [];
+  }
+
+  enqueue(val) {
+    this.leftStack.push(val);
+  }
+
+  dequeue() {
+    this.prepare();
+    return this.rightStack.pop();
+  }
+
+  prepare() {
+    if (!this.rightStack.length) {
+      while (this.leftStack.length) {
+        let value = this.leftStack.pop();
+        this.rightStack.push(value);
+      }
+    }
+  }
+
+  size() {
+    return this.leftStack.length + this.rightStack.length;
+  }
+}
