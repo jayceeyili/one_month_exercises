0fccb2dead74b78fab54188e33361e993773e4a0
Li Yi <jayceeyili@gmail.com>
2017-08-17 14:17:18 -0700

    initail implementation of is_shuffle

--- coding_exercises/is_shuffle.js
+++ coding_exercises/is_shuffle.js
@@ -11,3 +11,22 @@
   => true
   Note: make sure you can answer why this won't work with your initial implementation.
 ***************************************************************************************************/
+
+// initial assumption: no dup
+const is_shuffle1 = (str1, str2, target) => {
+  let idx1 = 0;
+  let idx2 = 0;
+  target.split('').forEach(char => {
+    if (char === str1[idx1]) {
+      idx1++;
+    } else if (char === str2[idx2]) {
+      idx2++;
+    }
+  })
+  return idx1 === str1.length && idx2 === str2.length;
+};
+
+// with repeats:
+const is_shuffle = (str1, str2, target) => {
+  
+};
