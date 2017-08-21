b3042a58df206aa1408e5a343af7a824f70d5c31
Li Yi <jayceeyili@gmail.com>
2017-08-21 13:25:28 -0700

    reimplement binary

--- coding_exercises/binary.js
+++ coding_exercises/binary.js
@@ -2,6 +2,10 @@
   Write a function that takes an integer and returns it in binary form.
 ***************************************************************************************************/
 
+// -3 >>> 0 (right logical shift) coerces its arguments to unsigned integers,
+// which is why you get the 32-bit two's complement representation of -3.
+// const binary = num => (num >>> 0).toString(2);
+
 const binary = num => {
   let result = '';
 
