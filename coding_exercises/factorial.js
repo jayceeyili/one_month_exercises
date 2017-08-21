077d74901ecc6f3515905767995a3e84710a9a17
Li Yi <jayceeyili@gmail.com>
2017-08-21 13:29:16 -0700

    complete factorial recusively

--- coding_exercises/factorial.js
+++ coding_exercises/factorial.js
@@ -4,3 +4,14 @@
   What is tail-recursion? Do Ruby or JavaScript have tail-call optimization? Pretend they do;
   write a tail-recursive version of rec_fac.
 ***************************************************************************************************/
+
+// const factorial = num => {
+//   if (num === 1) return num;
+//   return num * factorial(num - 1);
+// };
+
+const factorial = num => {
+  
+};
+
+console.log(factorial(5));
