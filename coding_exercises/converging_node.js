5bb1b04bff38214d61ed0b8ea94ec6e37184cf5b
Li Yi <jayceeyili@gmail.com>
2017-08-22 13:32:50 -0700

    add descriptions for exercises

---
+++ coding_exercises/converging_node.js
@@ -0,0 +1,4 @@
+/***************************************************************************************************
+  Given two singly-linked lists of (possibly) differing lengths that converge at some point,
+  find the node at which they converge.
+***************************************************************************************************/
---
+++ coding_exercises/permutations.js
@@ -0,0 +1,11 @@
+/***************************************************************************************************
+  Write a method that takes an array and returns all its permutations.
+  Time/memory complexity should be proportional to the number of permutations; what is this?
+  Example:
+  permutations([1,2,3])
+  => [[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
+  Bonus:
+  Write a class, PermutationIterator, that will iterate through permutations of an array.
+  It should use O(n) memory, and return the "next" permutation in O(1) time.
+  It can iterate through permutations in whatever order you desire.
+***************************************************************************************************/
