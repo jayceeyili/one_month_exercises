aace4d888314e9893bd0a514833a859d4dd36230
Li Yi <jayceeyili@gmail.com>
2017-08-22 15:59:04 -0700

    complete truckin

--- coding_exercises/truckin.js
+++ coding_exercises/truckin.js
@@ -3,3 +3,13 @@
   how far can you deliver a payload? You can transfer the payload from truck to truck,
   and you can transfer fuel from truck to truck. Assume all the payload will fit in one truck.
 ***************************************************************************************************/
+
+const truckin = (fleetSize, perRange, totalMiles) => {
+  totalMiles = totalMiles || perRange / fleetSize;
+  if (fleetSize === 1) return totalMiles;
+  fleetSize--;
+  totalMiles += perRange / fleetSize;
+  return truckin(fleetSize, perRange, totalMiles);
+};
+
+console.log(truckin(40, 80));
