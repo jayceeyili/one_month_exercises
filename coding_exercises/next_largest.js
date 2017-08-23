1d316fcf0505fe93f355ed5f050254eec71d6c37
Li Yi <jayceeyili@gmail.com>
2017-08-23 14:30:44 -0700

    complete next_largest

--- coding_exercises/next_largest.js
+++ coding_exercises/next_largest.js
@@ -2,3 +2,31 @@
   Given a node in a Binary Search Tree, find the node with the next largest value.
   Assume you don't have the root of the tree, just a single node from it.
 ***************************************************************************************************/
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const findNextLargest = node => {
+  if (node.right) {
+    node = node.right;
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  } else {
+    while (node.parent) {
+      if (node.parent.left === node) {
+        return node.parent;
+      }
+      node = node.parent;
+    }
+  }
+
+  return null;
+};
