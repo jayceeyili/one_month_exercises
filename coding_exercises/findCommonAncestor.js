a927da3e544ccb1fd5ec22bea31a0708892d9324
Li Yi <jayceeyili@gmail.com>
2017-08-23 14:58:21 -0700

    complete findCommonAncestor recursivly

--- coding_exercises/findCommonAncestor.js
+++ coding_exercises/findCommonAncestor.js
@@ -2,3 +2,21 @@
   Write a function to find the lowest common ancestor of two nodes in a binary search tree.
   Assume I give you both the root and the two nodes.
 ***************************************************************************************************/
+
+const findCommonAncestor = (root, node1, node2) => {
+  if (!root) return;
+
+  let val = root.value;
+  let val1 = node1.value;
+  let val2 = node2.value;
+
+  if (val1 < val && val2 < val) {
+    return findCommonAncestor(root.left, node1, node2);
+  }
+
+  if (val1 > val && val2 > val) {
+    return findCommonAncestor(root.right, node1, node2);
+  }
+
+  return root;
+};
