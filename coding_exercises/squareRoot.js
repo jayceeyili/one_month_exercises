6b268073b31705f7e80ca3bcf9b824dd8cfc3f7c
Li Yi <jayceeyili@gmail.com>
2017-08-24 14:24:58 -0700

    complete squareRoot using binary search

--- coding_exercises/squareRoot.js
+++ coding_exercises/squareRoot.js
@@ -4,3 +4,36 @@
   Hint: One naive solution has a better time complexity than many people realize--iterating
   from 0 until the square root is going to be O(sqrt n), not O(n). However, we can do better!
 ***************************************************************************************************/
+
+// naive solution
+// const squareRoot = num => {
+//   for (let i = 1; i <= num / 2; i++) {
+//     if (i * i === num) return i;
+//   }
+// };
+
+// use binary search
+const squareRoot = num => {
+  let tempArr = [];
+  for (let i = 1; i <= num / 2; i++) tempArr.push(i);
+
+  const search = (low, high) => {
+    if (low === high || low === high - 1) return null;
+
+    let mid = Math.floor((high - low) / 2) + low;
+
+    if (tempArr[mid] * tempArr[mid] === num) {
+      return mid;
+    } else if (num < tempArr[mid] * tempArr[mid]) {
+      return search(low, mid);
+    } else {
+      return search(mid, high);
+    }
+  };
+
+  let idx = search(0, tempArr.length - 1);
+
+  return tempArr[idx];
+};
+
+console.log(squareRoot(121));
