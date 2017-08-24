ac2e85ba92adb431a23a61548d02b39d5b64d8c4
Li Yi <jayceeyili@gmail.com>
2017-08-24 15:44:04 -0700

    pseudocode for sudoku

--- coding_exercises/rand7.js
+++ coding_exercises/rand7.js
@@ -3,3 +3,7 @@
   given a method that generates a random integer between (0...5).
   The resulting rand7 distribution must be uniform.
 ***************************************************************************************************/
+
+const gen5 = () => Math.floor(Math.random() * 6);
+
+console.log(gen5());
--- coding_exercises/sudoku.js
+++ coding_exercises/sudoku.js
@@ -1,3 +1,21 @@
 /***************************************************************************************************
   How would you write a program to solve Sudoku? (high level description)
 ***************************************************************************************************/
+
+// The main idea is called "backtracking".
+const solve = board => {
+  // base case: if all cell are filled, return true;
+  if (allFilled(board)) {
+    return true;
+  } else {
+    next_cell = getNextEmptyCell(board);
+
+    allPossiableValidNumber.forEach(num => {
+      toggle(board, next_cell, num);
+      if (solve(board)) return true;
+      toggle(board, next_cell, num);
+    })
+  }
+
+  return false;
+};
