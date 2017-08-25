75490bc0af52dc9c3d4d2c7c812844721ac795ff
Li Yi <jayceeyili@gmail.com>
2017-08-25 15:37:29 -0700

    update decription

--- week3_projects/Dijkstra_Algorithm.js
+++ week3_projects/Dijkstra_Algorithm.js
@@ -1,3 +1,4 @@
 /***************************************************************************************************
- Implement Dijkstra's Algorithm, which finds the shortest rought from one node to another in a graph
+ Implement Dijkstra's Algorithm, which finds the most cost efficient rought
+ from one node to another in a graph
 ***************************************************************************************************/
