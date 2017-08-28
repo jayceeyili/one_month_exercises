1dad507f228b126d7ac81d108b299de0e38298f0
Li Yi <jayceeyili@gmail.com>
2017-08-28 12:58:50 -0700

    add description for w4d1 exercises

---
+++ coding_exercises/wordChains.js
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+  Given a source word, target word and an English dictionary, transform the source word to target
+  by changing/adding/removing 1 character at a time, while all intermediate words being valid
+  English words. Return the transformation chain which has the smallest number of intermediate words.
+***************************************************************************************************/
+
+const wordChains = (source, target, dictionary) => {
+  
+};
