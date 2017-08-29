e7b8b582df0a6bdd490591a11af69d93b8edb584
Li Yi <jayceeyili@gmail.com>
2017-08-29 15:52:51 -0700

    add description for median

---
+++ coding_exercises/median.js
@@ -0,0 +1,11 @@
+/***************************************************************************************************
+  Given two sorted arrays, find the median element amongst the two arrays.
+  That is, if both arrays were combined, find the median element from the combined array.
+  Assume that there is not enough memory to actually combine both arrays.
+  There exists an O(log n + log m) solution.+
+***************************************************************************************************/
+
+// binary search
+const median = (arr1, arr2) => {
+  
+};
