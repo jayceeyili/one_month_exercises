533791582cdec2b146f4990817208914999dad3b
Li Yi <jayceeyili@gmail.com>
2017-08-29 13:45:12 -0700

    complete streamSample with help on stack overflow

--- coding_exercises/streamingSample.js
+++ coding_exercises/streamingSample.js
@@ -4,3 +4,23 @@
   Use only O(1) memory.
   Every value in the stream should have an equal probability of being sampled.
 ***************************************************************************************************/
+
+const streamSample = stream => {
+  /*
+    Consider a sequence of three elements. The first one is picked with a probability of 1.
+    However, it might be replaced (notice that we don't return immediately!) by the second
+    element with a probability of 1/2. The second element might in turn be replaced by
+    the third element, with a probability of 1/3.
+    So we get P(first) = P(first picked) * P(second not picked) * P(third not picked) = 1 * 1/2 * 2/3 = 1/3;
+    P(second) = P(second picked) * P(third not picked) = 1/2 * 1/3 = 1/3;
+    P(third) = P(third picked) = 1/3.
+  */
+  let result;
+  let count = 0;
+  for (let key in stream) {
+    if (Math.random() < 1 / ++count) {
+      result = key;
+    }
+  }
+  return stream[key];
+};
