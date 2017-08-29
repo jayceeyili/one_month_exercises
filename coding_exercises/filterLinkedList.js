beb76031e0b811ce24106fb2a861f90382eb9a48
Li Yi <jayceeyili@gmail.com>
2017-08-29 13:07:33 -0700

    add description for w4d2 exercises

---
+++ coding_exercises/filterLinkedList.js
@@ -0,0 +1,11 @@
+/***************************************************************************************************
+  Write a double-ended LinkedList class in JavaScript.
+  You should have a Link class
+  It should keep a reference to next and prev.
+  You should have a LinkedList class
+  It should have first and last methods to return the first/last links in the list, 
+  or undefined if the list is empty.
+  It should have push and pop methods.
+  You should write a remove method that takes in a value and removes the first link found with that value.
+  Given a linked list of integers and an integer value, delete every node of the linkedlist containing that value.
+***************************************************************************************************/
