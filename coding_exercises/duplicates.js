7d3eb5b9a75529be7cb7a639941fa6f8ff49b5a2
Li Yi <jayceeyili@gmail.com>
2017-08-30 12:37:34 -0700

    complete duplicates

--- coding_exercises/duplicates.js
+++ coding_exercises/duplicates.js
@@ -1,3 +1,17 @@
 /***************************************************************************************************
   Write a method that takes an array and returns its duplicate values. Use less than O(n*n) time.
 ***************************************************************************************************/
+
+const duplicates = arr => {
+  let countMap = arr.reduce((a, b) => {a[b]++ ? 0 : a[b] = 1; return a}, {});
+  let result = [];
+
+  for (let key in countMap) {
+    if (countMap[key] > 1) result.push(key);
+  }
+
+  return result;
+};
+
+let a = [1, 2, 4, 4, 5, 6, 7, 5];
+console.log(duplicates(a));
