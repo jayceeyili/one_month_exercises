f79e534c0b1785588d3a700be9045552dac83b21
Li Yi <jayceeyili@gmail.com>
2017-08-30 12:56:46 -0700

    conplete choose_a_record

--- coding_exercises/choose_a_record.js
+++ coding_exercises/choose_a_record.js
@@ -4,3 +4,23 @@
   The method returns a key such that the chances of selecting a particular key are weighted by that key's value.
   For the hash {:a => 1, :b => 2, :c => 3}, the chance of returning :c is 1/2, :b is 1/3, and :a is 1/6.
 ***************************************************************************************************/
+
+const choose_a_record = hash => {
+  let totleWeight = Object.values(hash).reduce((a, b) => a + b);
+  let count = 0;
+
+  for (let key in hash) {
+    count += hash[key];
+    if (Math.random() * totleWeight < count) {
+      return key;
+    }
+  }
+};
+
+let hash = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+
+console.log(choose_a_record(hash));
