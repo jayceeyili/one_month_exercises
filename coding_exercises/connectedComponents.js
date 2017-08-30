f63488babc70c4a432c44bb79b0aec82a9fcc01e
Li Yi <jayceeyili@gmail.com>
2017-08-30 16:13:47 -0700

    attempt connectedComponents

--- coding_exercises/connectedComponents.js
+++ coding_exercises/connectedComponents.js
@@ -11,3 +11,30 @@
   In this example, the connected components are [["AA", "BB"], ["CC", "DD", "EE", "FF"]].
   You don't have to return the elements of the components in any particular order.
 ***************************************************************************************************/
+
+const fs = require('fs');
+
+const connectedComponents = file => {
+  let record = fs.readFileSync(file, 'utf8');
+  let lines = record.split('\n').filter(e => e).map(e => e.split(' '));
+  let vertexMap = {};
+
+  // lines.forEach(line => {
+  //   vertexMap[line[0]] = line[1];
+  //   vertexMap[line[1]] = line[0];
+  // })
+  //
+  // let result = [];
+  // for (let key in vertexMap) {
+  //   let lastResult = result[result.length - 1] || [];
+  //   lastResult.push(vertexMap[key]);
+  //   if (vertexMap[vertexMap[key]]) {
+  //     lastResult.push(key);
+  //   }
+  //   result.push(lastResult);
+  // }
+
+  return result;
+};
+
+console.log(connectedComponents('/Users/yili/Desktop/10x_Club_Exercises/coding_exercises/testFile.txt'));
