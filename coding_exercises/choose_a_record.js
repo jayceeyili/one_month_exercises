0b5eab51fb5783033b17432a0b5438b45031afc8
Li Yi <jayceeyili@gmail.com>
2017-09-06 12:52:06 -0700

    refactor choose_a_record

--- coding_exercises/choose_a_record.js
+++ coding_exercises/choose_a_record.js
@@ -8,10 +8,11 @@
 const choose_a_record = hash => {
   let totleWeight = Object.values(hash).reduce((a, b) => a + b);
   let count = 0;
+  let rand = Math.random() * totleWeight;
 
   for (let key in hash) {
     count += hash[key];
-    if (Math.random() * totleWeight < count) {
+    if (rand < count) {
       return key;
     }
   }
