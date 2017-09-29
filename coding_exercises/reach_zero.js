08fbf5df21d5804eba4503f9531ab20172d1b7f4
Li Yi <jayceeyili@gmail.com>
2017-09-28 22:17:41 -0700

    add decription

---
+++ coding_exercises/reach_zero.js
@@ -0,0 +1,7 @@
+/***************************************************************************************************
+  Define a function which takes an array and a start index as input, return true or false depends on
+  if the pointer can reach a '0'.
+  EX: canWin([0, 1, 2, 3, 4], 3) => true; because the start index is 3, which the value is 3; so it
+  can move left or right for 3 posion; for left, it reaches 0, so return true;
+  canWin([0, 2, 3, 5, 23], 2) => false; 2 => 3 => false;
+***************************************************************************************************/
